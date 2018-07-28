2ad4b397b8f59e3a57f08bd2c4efb0cc8224b7c4
paulosilvino23 <paulosilvino23@hotmail.com>
2018-07-28 11:58:03 -0300

    Implementing the StartApp.java file

--- StartApp.java
+++ StartApp.java
@@ -2,7 +2,19 @@
 public class StartApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		// System.out.println("Hello World");
+		
+		Person p1 = new Person();
+		p1.setName("Paulo Silvino");
+		p1.setCpf("99001102010");
+		p1.setGender(new Character('M'));
+
+		System.out.println("*** START ***");
+				
+		System.out.println(p1.getName() + ", " + p1.getCpf() + ", " + p1.getGender());
+
+		System.out.println("*** FINISH ***");
 	}
 
 }
