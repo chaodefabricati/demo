a50a947dd359f47200e9b83a5db3e297d53db276
paulosilvino23 <paulosilvino23@hotmail.com>
2018-07-28 11:51:07 -0300

    Employee class implementation

--- Employee.java
+++ Employee.java
@@ -1,4 +1,23 @@
-public class Employee {
+public class Employee extends Person {
 
 	// put the implementation here
+	
+	private String socialNumber;
+	private String pisNumber;
+
+	public String getSocialNumber() {
+		return this.socialNumber;
+	}
+
+	public String getPisNumber() {
+		return this.pisNumber;
+	}
+
+	public void setSocialNumber(String socialNumber) {
+		this.socialNumber = socialNumber;
+	}
+
+	public void setPisNumber(String pisNumber) {
+		this.pisNumber = pisNumber;
+	}
 }
